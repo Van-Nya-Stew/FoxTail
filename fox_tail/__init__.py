f8176588b81a7c57964851649ae63df94f9fcee1
Van_Nya <nya@ruavan.one>
2021-08-19 22:23:51 +0800

    Update translated command output

--- fox_tail/__init__.py
+++ fox_tail/__init__.py
@@ -6,12 +6,11 @@ from fox_tail.plug import plug_in_fox
 
 
 def on_load(server: PluginServerInterface, old):
-    server.logger.info(server.tr('prprpr'))
     server.register_help_message('!!fox', server.tr('help_msg'))
     server.register_command(Literal('!!fox').runs(plug_in_fox(RTextList(
         '\\',
-        RText('fu哩', RColor.gold)
+        RText(server.tr('fox'), RColor.gold)
         .h('Fallen_Breath')
         .c(RAction.open_url, 'https://github.com/Fallen-Breath'),
-        'yi叭柿奇怪塞子/'
+        server.tr('tail'), '/'
     ))))
